3e31f7bab4284c3db5682ca93e51c7f17c942eff
Elijah <Elijah@ELIJAH>
2024-04-12 16:39:22 +0800

    Add project files.

---
+++ MenuPlanningUI/Program.cs
@@ -0,0 +1,43 @@
+﻿using MenuPlanningBL;
+using MenuPlanningDL;
+using MenuPlanning;
+namespace MenuPlanningUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("\nMenu Plan for a Day");
+                Console.WriteLine("1. Information about the program");
+                Console.WriteLine("2. Proceed to Program");
+                Console.WriteLine("3. Exit");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("This program will let you pick which meal option you want and will show the recommended dishes, beverages, preference, nutritional value and time when you will eat");
+                        break;
+
+                    case "2":
+
+                        Data.SelectDay();
+
+                        break;
+
+                    case "3":
+                        exit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong Input. Please Try Again");
+                        break;
+                }
+
+            }
+        }
+    }
+}
